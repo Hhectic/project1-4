7601fe98456df7f3a502b6b81eec28580b5573f6
Hhectic <richardh5@nycstudents.net>
2022-12-06 11:09:24 -0500

    Initial commit

---
+++ src/quiz.java
@@ -0,0 +1,151 @@
+import java.util.Scanner;
+/*The quiz class represents a quiz. A quiz is represented with questions, user answers, the actual answers, number of questions, and
+number of questions answered correctly
+ */
+public class quiz {
+    Scanner answers = new Scanner(System.in);
+
+    private String userA;
+    private int qNum;
+    private int numCorr;
+    private String q1;
+    private String q2;
+    private String q3;
+    private String q4;
+    private String a1;
+    private String a2;
+    private String a3;
+    private String a4;
+
+    /*Constructor for the quiz class. This creates a new instance of a quiz given no parameters
+     */
+    public quiz() {
+        qNum = (int) (Math.random() * 5 + 1);
+        numCorr = 0;
+        q1 = "What is the absolute value of -9?";
+        q2 = "If the English alphabet goes from A - Z, what goes from Z - A?";
+        q3 = "Who won the space race?";
+        q4 = "Whats the square root of 36?";
+        a1 = Integer.toString(Math.abs(-9));
+        a2 = "Zebra";
+        a3 = "USA";
+        a4 = Integer.toString((int) Math.sqrt(36));
+    }
+
+    /*Constructor for the quiz class. This creates a new instance of a quiz given the amount of questions a user wants
+     */
+    public quiz(int qNum) {
+        this.qNum = qNum;
+        numCorr = 0;
+        q1 = "What is the absolute value of -9?";
+        q2 = "If the English alphabet goes from A - Z, what goes from Z - A?";
+        q3 = "Who won the space race?";
+        q4 = "Whats the square root of 36?";
+        a1 = Integer.toString(Math.abs(-9));
+        a2 = "Zebra";
+        a3 = "USA";
+        a4 = Integer.toString((int) Math.sqrt(36));
+    }
+
+
+    /* boolean methods used to check if the users answer matches with the answer, not case-sensitive (this is how it works from correct1 all the way to correct4)
+    */
+    public boolean correct1(String userA){
+        this.userA = userA;
+        if(userA.toLowerCase().equals(a1.toLowerCase())){
+            return true;
+        }
+        return false;
+    }
+    public boolean correct2(String userA){
+        this.userA = userA;
+        if(userA.toLowerCase().equals(a2.toLowerCase())){
+            return true;
+        }
+        return false;
+    }
+    public boolean correct3(String userA){
+        this.userA = userA;
+        if(userA.toLowerCase().equals(a3.toLowerCase())){
+            return true;
+        }
+        return false;
+    }
+    public boolean correct4(String userA){
+        this.userA = userA;
+        if(userA.toLowerCase().equals(a4.toLowerCase())){
+            return true;
+        }
+        return false;
+    }
+
+    /* void method with no parameters, when called it prints out the questions and incorporates the boolean methods correct1 through correct4 in order to check
+    if the user input is correct. After the user completes the quiz it prints out the amount of questions the user got right out of the number they answered
+
+     */
+    public void qAndA(){
+        for (int i = 1; i <= qNum; i++){
+            if(i==1){
+                System.out.println(q1);
+                userA = answers.nextLine();
+                if(correct1(userA) == true){
+                    numCorr++;
+                } else {
+                    System.out.println("Wrong");
+                }
+            }
+            if(i==2){
+                System.out.println(q2);
+                userA = answers.nextLine();
+                if(correct2(userA) == true){
+                    numCorr++;
+                } else {
+                    System.out.println("Wrong");
+                }
+            }
+            if(i==3){
+                System.out.println(q3);
+                userA = answers.nextLine();
+                if(correct3(userA) == true || userA.toLowerCase().equals("us") || userA.toLowerCase().equals("united states") || userA.toLowerCase().equals("united states of america")){
+                    numCorr++;
+                } else {
+                    System.out.println("Wrong");
+                }
+            }
+            if(i==4){
+                System.out.println(q4);
+                userA = answers.nextLine();
+                if( userA.equals("-6") || correct4(userA) == true ){
+                    numCorr++;
+                } else {
+                    System.out.println("Wrong");
+                }
+            }
+
+
+        }
+        if (numCorr <= qNum/2){
+            System.out.println("You only  " + numCorr + " out of " + qNum + ", you can do better, want to try again?");
+        } else if (numCorr == qNum || numCorr == qNum - 1){
+            System.out.println("You got  " + numCorr + " out of " + qNum + " correct, you did great!");
+        } else {
+            System.out.println("You got" + numCorr + " out of " + qNum + " correct, you did ok!");
+        }
+    }
+
+    /*
+    toString method modified to print out the answer key for the quiz
+     */
+
+    public String toString()
+    {
+        String returnString = "Answer key: " + "\n" +  "question 1 answer is " + a1 + "\n" + "question 2 answer is " + a2 + "\n" + "question 3 answer is " + a3 + "\n" + "question 4 answer is " + a4 + " or -6";
+
+        return returnString;
+    }
+
+
+
+
+
+}
---
+++ src/quizRunner.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class quizRunner {
+    public static void main(String[]args) {
+        Scanner qNumb = new Scanner(System.in);
+        System.out.println("How many questions do you want to answer out of 4?" + "\n" + " Type R for a random amount of questions");
+        String questionNum = qNumb.nextLine();
+        if (questionNum.toLowerCase().equals("r")){
+            quiz q1 = new quiz();
+        } else {
+            while (!(questionNum.equals("1") || questionNum.equals("2") || questionNum.equals("3") || questionNum.equals("4"))) {
+                System.out.println(questionNum);
+                System.out.println("Invalid number, retry");
+                System.out.println("How many questions do you want to answer out of 4?" + "\n" + " Type R for a random amount of questions");
+                questionNum = qNumb.nextLine();
+            }
+        }
+        quiz q1 = new quiz();
+        if (!questionNum.equals("R")) {
+            int q = Integer.parseInt(questionNum);
+            q1 = new quiz(q);
+        }
+        q1.qAndA();
+
+        System.out.println("Would you like the answer key for all questions? Type yes if you do");
+        String ua = qNumb.nextLine();
+        if (ua.toLowerCase().equals("yes")) {
+            System.out.println(q1);
+        }
+
+    }
+}
